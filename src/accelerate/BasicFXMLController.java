937b49c2c6658af9b315f2c250c995cac40dcfa6
moyernwentworth <43347442+moyernwentworth@users.noreply.github.com>
2018-09-24 15:08:28 -0400

    its getting better

--- src/accelerate/BasicFXMLController.java
+++ src/accelerate/BasicFXMLController.java
@@ -1,21 +1,29 @@
 
 package accelerate;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class BasicFXMLController {
-	
+	String producty = null;
+
      Stage stage;
      Parent root;
     
     @FXML
+    private ImageView productPage;
+    
+    @FXML
     private MenuBar menu;
+    
 
     @FXML
     private MenuBar profile;
@@ -28,9 +36,16 @@ public class BasicFXMLController {
     
     @FXML
     private MenuBar profileBack;
-
+    
+    @FXML
+    private MenuBar upload;
+    
+    @FXML
+    private Button saveProfile;
+    
     @FXML
     void handleMenu(MouseEvent event) {
+
     }
 
     
@@ -69,5 +84,37 @@ public class BasicFXMLController {
        stage.show();                  //displays it
       
 }
+    @FXML
+    void handleProductPageName(MouseEvent event) throws Exception {
+    	producty = "ProductPage";// have to make that work
+    }
+    @FXML
+    void handleProductPage(MouseEvent event) throws Exception {
+    	String productx = producty +".fxml";
+		if(event.getSource()==productPage){
+            stage = (Stage) productPage.getScene().getWindow();  //grabs the current stage (by looking at the window that the button belongs to
+            root = FXMLLoader.load(getClass().getResource(productx)); //loads the other scene fxml
+    
+        }
+        
+       Scene scene = new Scene(root); //instantiates new scene
+       stage.setScene(scene);         //sets the scene to the stage
+       stage.show();                  //displays it
+    }
+    @FXML
+    void handleUpload(MouseEvent event) throws Exception{
+    	if(event.getSource()==upload){
+            stage = (Stage) upload.getScene().getWindow();  //grabs the current stage (by looking at the window that the button belongs to
+            root = FXMLLoader.load(getClass().getResource("jobs.fxml")); //loads the other scene fxml
+       
+        }
+        
+       Scene scene = new Scene(root); //instantiates new scene
+       stage.setScene(scene);         //sets the scene to the stage
+       stage.show();                  //displays it
+    }
+    void handleSaveProfile(ActionEvent event) throws Exception{
+    	System.exit(0);
+    }
     }
 
